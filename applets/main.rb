d69ba31a2cf324050888e4f6cdd27357ce62db77
Ray Renaldi <renaldi.sim@gmail.com>
2014-01-01 17:58:09 -0600

    changes on get_images path

--- applets/main.rb
+++ applets/main.rb
@@ -19,7 +19,14 @@ module HafezWeb
       end
                
       def get_images(type)
-        images = Dir['./public/images/inventory/' + type + '/*.jpg']          
+        # go through each file in the type vehicle folder and modified the file extension to uppercase
+        Dir['./public/images/inventory/' + type + '/*.*'].each do |f|
+          # check if all files have extension .JPG
+          if File.extname(f) != "JPG"
+            FileUtils.mv f, "#{File.dirname(f)}/#{File.basename(f,'.*')}.JPG"
+          end          
+        end
+        images = Dir['./public/images/inventory/' + type + '/*.JPG']          
       end
       
       def show_dirs
