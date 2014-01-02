148b1eab02d9aab96bbb35b3a955e298c1948c38
Ray Renaldi <renaldi.sim@gmail.com>
2014-01-01 22:03:26 -0600

    commit

--- applets/main.rb
+++ applets/main.rb
@@ -7,36 +7,34 @@ module HafezWeb
     helpers Sinatra::RedirectWithFlash    
     #helpers Sinatra::JSON           
     
+    #set base directory path for daemon use
+    @base_dir_path = Dir.pwd + 'home/rey/hafez-web/public/images/inventory/'
+    
     #set path for layout and views
     set :haml, :layout => :'layout'
     set :views, Proc.new { File.join(root, "../views") }        
-  
-    helpers do       
-            
-      def get_dir 
-        if Dir.pwd == '/'
-          # get working working directory
-          @base_dir_path = Dir.pwd + 'home/rey/hafez-web/public/images/inventory/'
-          dir = Dir[@base_dir_path + '*/']
-        else
-          # get all folders for listing (localhost)
-          dir = Dir['./public/images/inventory/' + '*/']
-        end                                          
-      end
-               
-      def get_images(type)  
-        @base_image_path = @base_dir_path + type             
-        # go through each file in the type vehicle folder and modified the file extension to uppercase
-        Dir[@base_image_path + '/*.*'].each do |f|
-          # check if all files have extension .JPG
-          if File.extname(f) != ".JPG"
-            FileUtils.mv f, "#{File.dirname(f)}/#{File.basename(f,'.*')}.JPG"
-          end          
-        end
-        images = Dir[@base_image_path + '/*.JPG']          
-      end
-            
+                      
+    def get_dir 
+      if Dir.pwd == '/'
+        # get working working directory          
+        dir = Dir[@base_dir_path + '*/']
+      else
+        # get all folders for listing (localhost)
+        dir = Dir['./public/images/inventory/' + '*/']
+      end                                          
     end
-  
+             
+    def get_images(type)  
+      @base_image_path = @base_dir_path + type             
+      # go through each file in the type vehicle folder and modified the file extension to uppercase
+      Dir[@base_image_path + '/*.*'].each do |f|
+        # check if all files have extension .JPG
+        if File.extname(f) != ".JPG"
+          FileUtils.mv f, "#{File.dirname(f)}/#{File.basename(f,'.*')}.JPG"
+        end          
+      end
+      images = Dir[@base_image_path + '/*.JPG']          
+    end
+                
   end        
 end
