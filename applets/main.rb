72aac33afa667d8bd8708b4da358b96d5a96af1e
Ray Renaldi <renaldi.sim@gmail.com>
2014-01-02 13:20:45 -0600

    commit

--- applets/main.rb
+++ applets/main.rb
@@ -14,7 +14,11 @@ module HafezWeb
     def base_path
       Dir.pwd + 'home/rey/hafez-web/public/images/inventory/'      
     end
-                          
+                 
+    def image_path(type)
+      "../public/images/inventory/#{type}/"  
+    end
+                              
     def get_dir            
       if Dir.pwd == '/'
         # get working working directory          
@@ -25,16 +29,20 @@ module HafezWeb
       end                                          
     end
              
-    def get_images(type)  
-      base_image_path = base_path + type             
-      # go through each file in the type vehicle folder and modified the file extension to uppercase
-      Dir[base_image_path + '/*.*'].each do |f|
-        # check if all files have extension .JPG
-        if File.extname(f) != ".JPG"
-          FileUtils.mv f, "#{File.dirname(f)}/#{File.basename(f,'.*')}.JPG"
-        end          
+    def get_images(type)              
+      if Dir.pwd == '/'
+        # go through each file in the type vehicle folder and modified the file extension to uppercase
+        Dir[image_path + '*.*'].each do |f|
+          # check if all files have extension .JPG
+          if File.extname(f) != ".JPG"
+            FileUtils.mv f, "#{File.dirname(f)}/#{File.basename(f,'.*')}.JPG"
+          end          
+        end        
+        images = Dir['./public/images/inventory/' + type + '/*.JPG']
+      else
+                
       end
-      images = Dir[base_image_path + '/*.JPG']          
+                
     end
                 
   end        
